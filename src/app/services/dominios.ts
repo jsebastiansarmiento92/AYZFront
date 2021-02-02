b4a3107b283aaeb7b4268455a92a322965f3e262
juan sebastian <=>
2021-02-01 22:18:57 -0500

    primer release agregado boton wsp

--- src/app/services/dominios.ts
+++ src/app/services/dominios.ts
@@ -1,5 +1,5 @@
-export let dominioAreasPracticas = 'http://localhost:3000/api/public/areasPracticas';
-export let dominioGrupoTrabajo = 'http://localhost:3000/api/public/grupoTrabajo';
-export let dominioGrupoAreas = 'http://localhost:3000/api/public/grupoAreas';
+export let dominioAreasPracticas = 'http://pruebasboyacaweb.tk/api/public/areasPracticas';
+export let dominioGrupoTrabajo = 'http://pruebasboyacaweb.tk/api/public/grupoTrabajo';
+export let dominioGrupoAreas = 'http://pruebasboyacaweb.tk/api/public/grupoAreas';
 export let servidor = 'http://pruebasboyacaweb.tk';
-export  let dominioUrl = 'http://localhost:3000/api/public/';
+export  let dominioUrl = 'http://pruebasboyacaweb.tk/api/public/';
